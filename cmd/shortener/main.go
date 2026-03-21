5aaf9bbecdbaa0955547d4699210fbd0d46f805a
Albert <albert@MacBook-Pro-Albert.local>
2026-03-21 18:04:09 +0300

    feat: Запустил сервер, написал валидациюдля проверки ссылки

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,72 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func validateUrl(inputUrl string) bool {
+	inputUrl = strings.TrimSpace(inputUrl)
+
+	if inputUrl == "" { 
+		return false 
+	}
+	
+	parsedUrl, err := url.ParseRequestURI(inputUrl)
+	if err != nil {
+		return false
+	}
+
+	if parsedUrl.Host == "" {
+		return false
+	}
+	
+	if parsedUrl.Scheme == "" {
+		return false
+	}
+
+	return true
+}
+
+func createShortenedUrl(inputUrl string) (string, error) {
+		if !validateUrl(inputUrl) {
+			return "", errors.New("Invalid URL")
+		}
+
+		return inputUrl, nil
+}
+
+
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+
+		inputUrl := r.FormValue("url") 
+
+		shortUrl, err := createShortenedUrl(inputUrl)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+
+		w.Write([]byte(shortUrl))
+	} else {
+		w.Write([]byte("GET"))
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(mainPage))
+
+	err := http.ListenAndServe("localhost:8080", mux)
+
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Albert-Ti/go-musthave-shortener-tpl
+
+go 1.25.4
