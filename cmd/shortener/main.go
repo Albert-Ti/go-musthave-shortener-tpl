d204f7b9ae4aee8d1cfce2c827f00dbe29413137
Albert <albert@MacBook-Pro-Albert.local>
2026-03-22 11:58:58 +0300

    feat: Вынес функцию обработчика mainPage => ./handler, а так реализовал получение ссылки по гет запросу

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,68 +1,19 @@
 package main
 
 import (
-	"errors"
 	"net/http"
-	"net/url"
-	"strings"
+
+	"github.com/Albert-Ti/go-musthave-shortener-tpl/internal/handler"
 )
 
-func validateUrl(inputUrl string) bool {
-	inputUrl = strings.TrimSpace(inputUrl)
-
-	if inputUrl == "" { 
-		return false 
-	}
-	
-	parsedUrl, err := url.ParseRequestURI(inputUrl)
-	if err != nil {
-		return false
-	}
-
-	if parsedUrl.Host == "" {
-		return false
-	}
-	
-	if parsedUrl.Scheme == "" {
-		return false
-	}
-
-	return true
-}
-
-func createShortenedUrl(inputUrl string) (string, error) {
-		if !validateUrl(inputUrl) {
-			return "", errors.New("Invalid URL")
-		}
-
-		return inputUrl, nil
-}
-
-
-
-func mainPage(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-
-		inputUrl := r.FormValue("url") 
-
-		shortUrl, err := createShortenedUrl(inputUrl)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-		
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusCreated)
-
-		w.Write([]byte(shortUrl))
-	} else {
-		w.Write([]byte("GET"))
-	}
-}
-
 func main() {
+	urls := &handler.ShortenedUrls{
+		List:  map[uint]string{},
+		Count: 1,
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.HandlerFunc(mainPage))
+	mux.Handle("/", http.HandlerFunc(handler.MainPage(urls)))
 
 	err := http.ListenAndServe("localhost:8080", mux)
 
---
+++ internal/handler/mainPage.go
@@ -0,0 +1,100 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+type ShortenedUrls struct {
+	List  map[uint]string
+	Count uint
+}
+
+func (u *ShortenedUrls) getUrl(key string) string {
+	for k, v := range u.List {
+		if "key_"+strconv.Itoa(int(k)) == key {
+			return v
+		}
+	}
+	return ""
+}
+func (u *ShortenedUrls) set(url string) string {
+	u.List[u.Count] = url
+
+	k := u.Count
+	u.Count++
+	return "key_" + strconv.Itoa(int(k))
+}
+
+func validateUrl(inputUrl string) bool {
+	inputUrl = strings.TrimSpace(inputUrl)
+
+	if inputUrl == "" {
+		return false
+	}
+
+	parsedUrl, err := url.ParseRequestURI(inputUrl)
+	if err != nil {
+		return false
+	}
+
+	if parsedUrl.Host == "" {
+		return false
+	}
+
+	if parsedUrl.Scheme == "" {
+		return false
+	}
+
+	return true
+}
+
+func createShortenedUrl(urls *ShortenedUrls, inputUrl string) (string, error) {
+	if !validateUrl(inputUrl) {
+		return "", errors.New("Invalid URL")
+	}
+
+	newKey := urls.set(inputUrl)
+
+	return newKey, nil
+}
+
+func MainPage(urls *ShortenedUrls) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+
+			inputUrl := r.FormValue("url")
+			shortUrl, err := createShortenedUrl(urls, inputUrl)
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			w.Header().Set("Content-Type", "text/plain")
+			w.WriteHeader(http.StatusCreated)
+
+			fullUrl := inputUrl + "/" + shortUrl
+			w.Write([]byte(fullUrl))
+		} else {
+			v := r.URL.Query().Get("key")
+			fmt.Println(v)
+			if v == "" {
+				http.Error(w, "Требуется ключ для получения ссылки", http.StatusBadRequest)
+				return
+			}
+
+			url := urls.getUrl(v)
+			if url == "" {
+				http.Error(w, "Ссылка по данному ключу не найдена", http.StatusBadRequest)
+				return
+			}
+
+			w.Write([]byte(url))
+		}
+	}
+}
