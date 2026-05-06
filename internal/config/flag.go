ff386134bce16aee7c70c58fb27d240b50b7096a
Albert <albert@MacBook-Pro-Albert.local>
2026-05-06 23:48:15 +0300

    fix: flag

--- internal/config/flag.go
+++ internal/config/flag.go
@@ -15,8 +15,8 @@ var Envs struct {
 func ParseFlag() {
 	flag.StringVar(&Envs.RunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&Envs.BaseURL, "b", "http://localhost:8080", "Base URL")
-	flag.StringVar(&Envs.FileStoragePath, "f", "shortenUrlList.json", "file storage")
-	flag.StringVar(&Envs.DatabaseDSN, "d", "postgres://postgres:postgres@localhost:5432/db", "connection string to DB")
+	flag.StringVar(&Envs.FileStoragePath, "f", "", "file storage")
+	flag.StringVar(&Envs.DatabaseDSN, "d", "", "connection string to DB")
 
 	flag.Parse()
 
